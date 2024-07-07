ea233b5b5d9ebb17b1ae34ea4d7a8eb25239ea8a
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-07-07 13:25:53 +0300

    Feat: Add RedisClient
    As to start of the storage of the API I will be using redis so had to create the RedisClient class which contains the basic connection and set & get methods.
    
    Alongside them is the __init__.py for ease of access of the RedisClient class.
    
    Files:
    - app/utils/__init__.py
    - app/utils/redis_client.py

---
+++ app/utils/redis_client.py
@@ -0,0 +1,57 @@
+import redis
+from flask import current_app
+
+class RedisClient:
+    """RedisClient
+
+    This class contains all the necessities for the redis client which
+    includes the connection creation to the setting of keys & values. 
+    """
+    def __init__(self, url=None) -> None:
+        """__init__
+        
+        Copying over the url for ease of access when using the client
+        and initalizing the redis client
+        
+        Argument:
+            url (str): The url to be passed to the redis client
+        """
+        self._url = url or current_app.config['REDIS_URL']
+        self.client = redis.StrictRedis.from_url(self._url)
+
+    def get_client(self):
+        """get_client
+
+        Returns the redis client
+        """
+        return self.client
+
+    def set(self, key, value):
+        """set
+        
+        This method sets into the redis db a given value assigned to
+        a given key and returns the success of that operation.
+
+        Arguments:
+            key (any): The key to be placed into the db.
+            value (any): The value to be assigned with that key.
+        
+        Return:
+            (int): The success of the operation.
+        """
+        return self.client.set(key, value)
+
+    def get(self, key):
+        """get
+        
+        This methods returns the value associated with a certain key
+        
+        Arguments:
+            key (any): The key associated with the value needed.
+        
+        Return:
+            (any): The value associated with the key if it's present
+        """
+        return self.client.get(key)
+
+redis_client = RedisClient()
