4057344e236bf6631bd400bea281cf7a9ed7ce2e
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-07-07 13:42:11 +0300

    Feat: Add redis_client object
    To fix the circular import i needed to remove the contents of __init__.py and move it over to the app.py as its still persisting to enter a circular import.
    
    Now that we have fixed the circular import we can initialize the redis_client inside the create_app method.
    
    Files:
    - app/__init__.py
    - app/app.py

--- app/app.py
+++ app/app.py
@@ -1,4 +1,14 @@
-from app import create_app
+from flask import Flask
+from config import Config
+from utils.redis_client import RedisClient
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    redis_client = RedisClient(app.config['REDIS_URL'])
+
+    return app
 
 if __name__ == "__main__":
     app = create_app()
